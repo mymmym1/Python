a1a899f58bafe554d9390936d0aa3414c635029b
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2018-08-20 11:04:12 -0400

    Create temp.py

---
+++ Programming for Chemists and Biochemists/HW1-3/temp.py
@@ -0,0 +1,9 @@
+f=raw_input("please input temp in degrees Fahrenheit: ")
+k=(int(f)-32) / (1.8)+273          
+c=(int(f)-32)/1.8
+
+print ("This temp in degrees Celsius is") 
+print c
+
+print ("This temp in degrees Kelvin is")
+print k
