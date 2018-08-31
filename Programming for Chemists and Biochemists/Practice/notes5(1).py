facb63a22369b9c0405efc34f1b09e98e8df62e6
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2018-08-31 18:44:58 -0400

    Add files via upload

---
+++ Programming for Chemists and Biochemists/Practice/notes5(1).py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+namelist=['Frank','Jim','Steve']
+gradelist=[[95,6,80.4,76.3],[75,6,90.4,66.3],[65.6,70.4,66.3]]
+
+if len(namelist)==len(gradelist):
+  for i in range(len(namelist)):
+     grades=gradelist[i]
+     sum=0
+     for grade in grades:
+        sum+=grade
+        avg=sum/len(gradelist)
+     print "%s's GPA is %1f"%(namelist[i],avg)
+
+else:
+    print "the number of names is different than the number of grades"
---
+++ Programming for Chemists and Biochemists/Practice/notes5(2).py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+namelist=[]
+gradelist=[]
+
+while True:
+      name=raw_input("Please enter a name or enter end to stop: ")
+      if name=='end':
+             break
+      gradestring=raw_input("Please enter a list of grades seperated by commas: ")
+      grades=[]
+      for n in gradestring.split(','):
+             grades.append(int(n))
+      namelist.append(name)
+      gradelist.append(grades)
+
+      if len(namelist)==len(gradelist):
+              for i in range(len(namelist)):
+                       grades=gradelist[i]
+                       sum=0
+                       for grade in grades:
+                                sum+=grade
+                       avg=sum/len(grades)
+                       print "%s's GPA is %.1f"%(namelist[i],avg)
+
